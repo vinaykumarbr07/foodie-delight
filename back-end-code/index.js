7a8f917ce28103feb71b2b198c5757448839363b
Vinay Kumar B R <vkumarbr@guidewire.com>
2024-07-18 09:41:05 +0530

    Add backend code and frontend simple code

---
+++ back-end-code/index.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { restaurant } from './restaurant-data.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+app.get('/restaurants',(req,res)=>{
+    res.json(restaurant);
+});
+
+app.post('/restaurants',(req,res)=> {
+    console.log(req.body)
+    restaurant.push(req.body);
+
+    res.json({
+        message: 'Restuarant data successfully added'
+    });
+})
+
+app.put('/restaurants/:id',(req,res)=>{
+    console.log(req.body)
+    const newData = req.body;
+    const resId = Number(req.params.id);
+
+    const restuarantIndex = restaurant.findIndex(restuarant=> restuarant.id === resId);
+
+    if (restuarantIndex !== -1) {
+        restaurant[restuarantIndex] = {
+            id: resId,
+            ...newData
+        }
+        res.json({
+            message: 'Restaurant data updated successfully'
+        })
+    } else {
+        res.status(400).json({
+            message: 'Resturant data not found'
+        })
+    }
+})
+
+app.delete('/restaurants/:id',(req,res)=> {
+    console.log(req.params.id)
+    const resId = Number(req.params.id);
+
+    const restuarantIndex = restaurant.findIndex(restuarant=> restuarant.id === resId);
+
+    if (restuarantIndex !== -1) {
+        restaurant.splice(restuarantIndex, 1);
+
+        res.json({
+            message: 'Restaurant data deleted successfully'
+        })
+    } else {
+        res.status(400).json({
+            message: 'Resturant not found'
+        })
+    }
+})
+
+const PORT = 5111;
+app.listen(PORT,(req,res)=>{
+    console.log(`Backend server started at port ${PORT}`);
+})
---
+++ back-end-code/restaurant-data.js
@@ -0,0 +1,13 @@
+export const restaurant = [{
+    "id": 100,
+    "name": "Kamath Hotel",
+    "description": "South Indian course",
+    "location": "Mallesh Palya"
+},
+{
+    "id": 101,
+    "name": "Udupi Hotel",
+    "description": "South Indian course",
+    "location": "Indira Nagar"
+}
+];
